7b1c4bc99b4a47dbfbc39473e4de87913e8b6886
Adeayo <aadelee74@gmail.com>
2024-10-25 22:10:49 +0100

    Refactor script import in index.html and add customer loading functionality in main.js

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-    
+    const mainContent = document.querySelector('#main-content');
+    const loadCustomersLink = document.querySelector('#load-customers');
+
+
+
+    function loadContent(url, callback){
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                mainContent.innerHTML = data;
+                callback();
+            });
+    }
+
+    loadCustomersLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        loadContent('./views/components/customer.html', () => {
+            console.log('Customers loaded');
+        });
+    });
 });
