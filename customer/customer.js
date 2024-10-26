e9d42159362118828c2232d4fea65a58b7119148
Adeayo <aadelee74@gmail.com>
2024-10-26 03:33:36 +0100

    Refactor customer script initialization and add form submission event listener

--- customer/customer.js
+++ customer/customer.js
@@ -1,12 +1,14 @@
-import {getCustomers} from "./handlers.js";
+import { handleSubmit, getCustomers } from "./handlers.js";
 
 export function initializeCustomerScripts() {
+  const customerForm = document.querySelector("#customer-form");
+  const customerList = document.querySelector("#customer-list");
 
-    getCustomers();
-    const customerForm = document.querySelector('#customer-form');
+  getCustomers(customerList);
 
-    
-
+  customerForm.addEventListener("submit", (e) => {
+    handleSubmit(e, customerForm, customerList);
+  });
 
   // ...
 }
