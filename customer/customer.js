0fb837befdd4cffd51765df42729ae09205f30ba
Adeayo <aadelee74@gmail.com>
2024-10-26 02:04:49 +0100

    Add customer data retrieval functionality  and initialized customer loading scripts

---
+++ customer/customer.js
@@ -0,0 +1,12 @@
+import {getCustomers} from "./handlers.js";
+
+export function initializeCustomerScripts() {
+
+    getCustomers();
+    const customerForm = document.querySelector('#customer-form');
+
+    
+
+
+  // ...
+}
