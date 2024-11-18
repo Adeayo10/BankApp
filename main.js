e9465f421912b9214465364cbf7f56ad6f7b123d
Adeayo <aadelee74@gmail.com>
2024-11-18 00:53:59 +0000

    Add user loading functionality to main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import { initializeCustomerScripts } from "./customer/customer.js";
+import { initializeUserScripts } from "./user/user.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const mainContent = document.querySelector("#main-content");
   const loadCustomersLink = document.querySelector("#load-customers");
+  const loadUserLink = document.querySelector("#load-users");
 
   function loadContent(url, callback) {
     fetch(url)
@@ -17,5 +19,11 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     loadContent("./views/components/customer.html", initializeCustomerScripts);
     console.log("Load customers clicked");
+  });
+
+  loadUserLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    loadContent("./views/components/user.html", initializeUserScripts);
+    console.log("Load users clicked");
   });
 });
