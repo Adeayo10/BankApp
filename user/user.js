ca190afabe5a426596ecb1d94618f49db7d8bb03
Adeayo <aadelee74@gmail.com>
2024-11-20 13:32:23 +0000

    Add delete user functionality to user scripts

--- user/user.js
+++ user/user.js
@@ -1,9 +1,10 @@
-import { handleGetUsers, handleCreateUser, handleUpdateUser } from "../user/handler.js";
+import { handleGetUsers, handleCreateUser, handleUpdateUser, handleDeleteUser } from "../user/handler.js";
 export function initializeUserScripts(){
     const userForm = document.getElementById("user-form");
     const userList = document.getElementById("user-list");
     const deleteUserButton = document.getElementById("delete-user");
     const updateUserButton = document.getElementById("update-user");
+   
 
     handleGetUsers(userList);
 
@@ -14,6 +15,10 @@ export function initializeUserScripts(){
     updateUserButton.addEventListener("click", () => {
         console.log("update user button clicked");
         handleUpdateUser(userForm, userList);
+    });
+
+    deleteUserButton.addEventListener("click", () => {
+        handleDeleteUser(userForm, userList);
     });
 
     
