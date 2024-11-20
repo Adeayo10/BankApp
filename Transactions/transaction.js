2a282e45757a46c02ed6429d9fbb94ba87221e00
Adeayo <aadelee74@gmail.com>
2024-11-20 15:09:19 +0000

    Initialize transaction scripts and fetch transactions for the transaction list

--- Transactions/transaction.js
+++ Transactions/transaction.js
@@ -1,4 +1,11 @@
+import {  handleGetTransactions } from "./handler.js";
 
 export function initializeTransactionScripts(){
-    
+
+    const transactionForm = document.getElementById("transaction-form");
+    const transactionList = document.getElementById("transaction-list");
+    const deleteTransactionButton = document.getElementById("delete-transaction");
+    const updateTransactionButton = document.getElementById("update-transaction");
+
+   handleGetTransactions(transactionList);
 }
