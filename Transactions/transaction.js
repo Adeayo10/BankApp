0b3f2ff930cf260178bf86dac6d856ba3fe8ab9a
Adeayo <aadelee74@gmail.com>
2024-11-20 17:23:43 +0000

    Update transaction handling to use customer terminology and add form submission functionality

--- Transactions/transaction.js
+++ Transactions/transaction.js
@@ -1,16 +1,19 @@
-import {  handleGetTransactions, displayUserName } from "./handler.js";
+import {  handleGetTransactions, displayCustomerName, handleTransactionFormSubmit} from "./handler.js";
 
 export function initializeTransactionScripts(){
 
     const transactionForm = document.getElementById("transaction-form");
     const transactionList = document.getElementById("transaction-list");
-    const deleteTransactionButton = document.getElementById("delete-transaction");
-    const updateTransactionButton = document.getElementById("update-transaction");
+    // const deleteTransactionButton = document.getElementById("delete-transaction");
+    // const updateTransactionButton = document.getElementById("update-transaction");
     const accountNumberInput = document.getElementById("transaction-customer-accountNumber");
+    const cancelTransactionButton = document.getElementById("cancel-transaction");
 
    handleGetTransactions(transactionList);
 
-    accountNumberInput.addEventListener("blur", displayUserName);
+    accountNumberInput.addEventListener("blur", displayCustomerName);
+    
+    transactionForm.addEventListener("submit", (e) => handleTransactionFormSubmit(e, transactionForm, transactionList));
   
-
+    cancelTransactionButton.addEventListener("click", () => transactionForm.reset());
 }
