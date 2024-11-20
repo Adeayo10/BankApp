a9482408e53bcda788787bdd388d1e4a1afe172e
Adeayo <aadelee74@gmail.com>
2024-11-20 14:18:08 +0000

    Add transaction loading functionality to main.js

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 import { initializeCustomerScripts } from "./customer/customer.js";
 import { initializeUserScripts } from "./user/user.js";
+import { initializeTransactionScripts } from "./transaction/transaction.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const mainContent = document.querySelector("#main-content");
   const loadCustomersLink = document.querySelector("#load-customers");
   const loadUserLink = document.querySelector("#load-users");
+  const loadTransactionsLink = document.querySelector("#load-transactions");
 
   function loadContent(url, callback) {
     fetch(url)
@@ -25,5 +27,14 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     loadContent("./views/components/user.html", initializeUserScripts);
     console.log("Load users clicked");
+  });
+
+  loadTransactionsLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    loadContent(
+      "./views/components/transaction.html",
+      initializeTransactionScripts
+    );
+    console.log("Load transactions clicked");
   });
 });
