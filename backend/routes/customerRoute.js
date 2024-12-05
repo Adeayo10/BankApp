9da2706180d2054cdbb8f151434f411109030c19
Adeayo10 <aadelee74@gmail.com>
2024-12-05 00:06:30 +0000

    Add customer route with CRUD operations

---
+++ backend/routes/customerRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { createCustomer, getCustomers, getCustomerById, updateCustomer, deleteCustomer } = require('../controllers/customerController');
+
+router.post('/create', createCustomer);
+router.get('/all', getCustomers);
+router.get('/:id', getCustomerById);
+router.put('/:id', updateCustomer);
+router.delete('/:id', deleteCustomer);
+
+module.exports = router;
