3a84a1e68beff048f65af794b63f69c14411c8cd
Adeayo10 <aadelee74@gmail.com>
2024-12-05 00:03:25 +0000

    Update database configuration to include DB_PORT and fix DB_PASSWORD variable

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,20 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, { host: process.env.DB_HOST, dialect: process.env.DB_DIALECT });    
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT, 
+  port: process.env.DB_PORT, 
+});
+
+db.authenticate()
+  .then(() => {
+    console.log('Database connected');
+  })
+  .catch((error) => {
+    console.log('Error connecting to database', error);
+  });
 
 module.exports = db;
