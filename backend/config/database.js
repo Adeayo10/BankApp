e8573116cd0ac49083b5bcd3da598b32b1144509
Adeayo10 <aadelee74@gmail.com>
2024-12-08 23:05:27 +0000

    Refactor database configuration to use Sequelize with updated dialect options and disable logging

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,20 +1,37 @@
+// const { Sequelize } = require('sequelize');
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+//   host: process.env.DB_HOST,
+//   dialect: process.env.DB_DIALECT, 
+//   port: process.env.DB_PORT, 
+// });
+
+// db.authenticate()
+//   .then(() => {
+//     console.log('Database connected');
+//   })
+//   .catch((error) => {
+//     console.log('Error connecting to database', error);
+//   });
+
+// module.exports = db;
+
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
 dotenv.config();
 
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT, 
-  port: process.env.DB_PORT, 
+  dialect: 'mysql',
+  dialectOptions: {
+    charset: 'utf8mb4',
+  },
+  logging: false,
 });
 
-db.authenticate()
-  .then(() => {
-    console.log('Database connected');
-  })
-  .catch((error) => {
-    console.log('Error connecting to database', error);
-  });
 
-module.exports = db;
+module.exports = sequelize;
