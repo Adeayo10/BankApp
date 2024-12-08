cdf8ba43728bcadcb60c2f1d41322d39743cd9da
Adeayo10 <aadelee74@gmail.com>
2024-12-08 23:06:29 +0000

    Refactor response message functions to use module exports for better accessibility and to accomodate commojs for jest test

--- backend/helpers/helpers.js
+++ backend/helpers/helpers.js
@@ -1,22 +1,32 @@
-export function successResponseMessage(res, message, data = null) {
+function successResponseMessage(res, message, data = null) {
   res.status(200).json({ status: "success", message, data });
 }
 
-export function createdResponseMessage(res, message) {
+function createdResponseMessage(res, message) {
   res.status(201).json({ status: "success", message });
 }
 
-export function errorResponseMessage(res, message) {
+function errorResponseMessage(res, message) {
   res.status(500).json({ status: "error", message });
 }
-export function notFoundResponseMessage(res, message) {
+
+function notFoundResponseMessage(res, message) {
   res.status(404).json({ status: "errors", message });
 }
 
-export function badRequestResponseMessage(res, message) {
+function badRequestResponseMessage(res, message) {
   res.status(400).json({ message });
 }
 
-export function unauthorizedResponseMessage(res, message) {
+function unauthorizedResponseMessage(res, message) {
   res.status(401).json({ message });
 }
+
+module.exports = {
+  successResponseMessage,
+  createdResponseMessage,
+  errorResponseMessage,
+  notFoundResponseMessage,
+  badRequestResponseMessage,
+  unauthorizedResponseMessage
+};
