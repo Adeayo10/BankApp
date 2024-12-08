4a3dd42531ad25af95a32f376b8ce7aac823c53f
Adeayo10 <aadelee74@gmail.com>
2024-12-08 02:09:35 +0000

    Fix case sensitivity in Customer model import in transaction service

--- backend/services/api/transactionservice.js
+++ backend/services/api/transactionservice.js
@@ -1,4 +1,4 @@
-const Customer = require('../../models/Customer');
+const Customer = require('../../models/customer');
 const Decimal = require('decimal.js');
 async function accountNumberExistAndReturnCustomer(accountNumber) {
   try {
