dd05094ab422eb3bc633cd6c1082107248dfb098
Adeayo10 <aadelee74@gmail.com>
2024-12-09 23:28:34 +0000

    Fix casing in customer model import for consistency

--- backend/services/api/transactionservice.js
+++ backend/services/api/transactionservice.js
@@ -1,4 +1,4 @@
-const Customer = require('../../models/customer');
+const Customer = require('../../models/Customer');
 const Decimal = require('decimal.js');
 async function accountNumberExistAndReturnCustomer(accountNumber) {
   try {
