e8b3f93b4f18d79e7394a0cc4fb0cc3f82c64b14
Adeayo10 <aadelee74@gmail.com>
2024-12-12 14:23:10 +0000

    refactor: clean up commented-out code in backend and frontend files; update README with setup instructions

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,24 +1,3 @@
-// const { Sequelize } = require('sequelize');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   dialect: process.env.DB_DIALECT, 
-//   port: process.env.DB_PORT, 
-// });
-
-// db.authenticate()
-//   .then(() => {
-//     console.log('Database connected');
-//   })
-//   .catch((error) => {
-//     console.log('Error connecting to database', error);
-//   });
-
-// module.exports = db;
-
 const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 
--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -1,35 +1,3 @@
-// const {DataTypes} = require('sequelize');
-// const db = require('../config/database');
-
-// const Transaction = db.define('transaction', {
-//     id: {
-//         type: DataTypes.INTEGER,
-//         primaryKey: true,
-//         autoIncrement: true
-//     },
-//     amount: {
-//         type: DataTypes.DECIMAL(10, 2),
-//         allowNull: false
-//     },
-//     type: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     date: {
-//         type: DataTypes.DATE,
-//         allowNull: false
-//     },
-//     accountNumber: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-//     description: {
-//         type: DataTypes.STRING,
-//         allowNull: false
-//     },
-// });
-// module.exports = Transaction;
-
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
